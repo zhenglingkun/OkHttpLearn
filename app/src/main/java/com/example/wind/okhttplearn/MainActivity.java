f5eb446ee20c7ef2556b911ad7e0889c9903aa87
zhenglingkun <zhenglingkun@163.com>
2016-05-24 01:46:53 +0800

    测试信息

--- app/src/main/java/com/example/wind/okhttplearn/MainActivity.java
+++ app/src/main/java/com/example/wind/okhttplearn/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // 这是一条测试信息
 
     }
 }
